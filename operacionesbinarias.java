4b7194d68bc63ba06c11ec60f0612fde8a6fb77e
Pablo Raul <45083864+pabloraul@users.noreply.github.com>
2018-11-27 19:45:40 -0300

    Update operacionesbinarias.java

--- operacionesbinarias.java
+++ operacionesbinarias.java
@@ -6,12 +6,17 @@ public class operacionesbinarias {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String sell=null;
-	int numero;
-	int exp=0;
-    int digito;
-    int resultado=0;
-    double binario;
+		
+	        String sell;
+		String stBinario;
+		char vecBinario[];
+		int Binario;
+		int posicionElevada;
+		int strLargo;
+		int numero;
+	        int exp=0;
+                int digito;
+	        boolean band;
         Scanner sec = new Scanner(System.in);
         System.out.println("1.-: CONVERSION DEC A BINARIO"
         		     + "\n 2.-: CONVERSION BINARIO A DEC");
@@ -21,42 +26,73 @@ public class operacionesbinarias {
         case("1"):
         	  System.out.println("A seleccionado DEC A BINARIO"
 					  + "\n Ingrese numero:");
-        numero = sec.nextInt();
-        while(numero<0);
+                 numero = sec.nextInt();
+                 while(numero<0);
 
-        exp=0;
-        binario=0;
-        while(numero!=0){
+		exp=0;
+		binario=0;
+		while(numero!=0){
         	
                 digito = numero % 2;            
                 binario = binario + digito * Math.pow(10, exp);   
                 exp++;
                 numero = numero/2;
-                
-        }
-        System.out.printf("Binario: %.0f %n", binario);
-        break;
-        
+
+		}
+		System.out.printf("Binario: %.0f %n", binario);
+		break;
+
         case("2"):
-        	
-        Scanner sed=new Scanner(System.in);
-        System.out.println("ingrese binario:");
-        numero=sed.nextInt();
-        do{
-        	digito=numero%10;
-        	resultado=resultado+digito*(int)Math.pow(2, exp);
-        	exp++;
-        	numero=numero/10;
-        	}
-       while(numero>0);
-        
-       System.out.println("DEC ES:"+resultado);
-        }
-        
-        
+        	double aux;
+		String mensajeError = "Numero binario NO VALIDO";
+		System.out.println("A seleccionado BINARIO A DEC"
+				  + "\n Ingrese numero BINARIO:");
+		Scanner in = new Scanner(System.in);
+		Binario=0;
+		aux=0;
+
+		do{
+		
+
+		stBinario = in.nextLine();
+		stLargo = strBinario.length();
+		vecBinario = new char[strLargo];
+
+		for (int i=0; i<strLargo; i++){ 
+		vecBinario[i]=strBinario.charAt(i);
+
+		}
+
+		band = false;
+		for (int j=0; j<strLargo; j++){
+		if (vecBinario[j] != '1' && vecBinario[j]!= '0'){
+
+		System.out.println(mensajeError);
+		j=strLargo;
+		band = true;
+
+		} else {
+		aux = Math.pow(2, (strLargo-1-j)); 
+		posicionElevada = (int) aux; 
+		digito = vecBinario[j]-'0'; 
+		Binario = Binario + digito*posicionElevada; 
+		band = false;
+		       }
+		    }
+		}
+	        while (band == true);
+		System.out.println("SU DECIMAL ES:"+Binario);
+       
+	
+
+		in.close();
+		   } 
         
    }
-}     		 
+}  
+        
+        
+        		 
 		
 		
         
