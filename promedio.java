52103948f95fa79a33db52f1acc364054ad87c0c
dvillanueva40 <44623641+dvillanueva40@users.noreply.github.com>
2018-12-07 21:15:25 -0200

    Update promedio.java

--- promedio.java
+++ promedio.java
@@ -6,36 +6,42 @@ public class promedio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	Scanner sed =  new Scanner(System.in);
-        int notas;
-        double nota;
-        double suma;
-        double prom=0;    
-        System.out.print("ingrese cantidad de notas:");//rango entre 10 y 70
-        notas = sed.nextInt();
-        
-        for(int i=1; i<= notas; i++){
-            System.out.println("ingrese nota"+" "+i+" "+"de"+" "+notas+":");
-
-            nota = sed.nextInt();  
-            
-            suma = 0;
-                 
-            suma = suma + nota;
-            prom= prom + suma /notas;
-             if (nota>70.0){System.out.println("error");
-             break;
-             }
-        else{System.out.println("promedio:"+prom );}
-            } 
-        
-      
-            
-            
-  }     
-
+	Scanner variable=new Scanner(System.in);
+		System.out.println("Ingrese cantidad de alumnos: ");
+		int alumnos=variable.nextInt();
 		
+		for(int k=1;k<=alumnos;k++){
+			System.out.println("Para alumno N° "+k+"\n");
+			Scanner sed =  new Scanner(System.in);
+	        int notas;
+	        double nota;
+	        double suma;
+	        double prom=0;    
+	        System.out.print("ingrese cantidad de notas:"+"\n"); //rango entre 10 y 70
+	        notas = sed.nextInt();
+	        
+	        for(int i=1; i<= notas; i++){
+	            System.out.println("ingrese nota"+" "+i+" "+"de"+" "+notas+":");
 
-}	
+	            nota = sed.nextInt();  
+	            
+	            suma = 0;
+	                 
+	            suma = suma + nota;
+	            prom= prom + suma /notas;
+	             if (nota>70.0){System.out.println("error");
+	             break;
+	             }
+	        else{System.out.println("promedio de alumno: "+k+" es "+prom+"\n" );}
+	            } 
+	        
+		}
+		
+		variable.close();
+		
+        
+	}
+
+}
 
 
